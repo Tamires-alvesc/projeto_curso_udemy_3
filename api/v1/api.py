384a58c25a21444489ae74eda53edddceef24ecb
Tamires-alvesc <ttamiresacosta@hotmail.com>
2025-10-19 16:32:18 -0300

    api atualizado com usuarios

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from api.v1.endpoints import artigo
+from api.v1.endpoints import usuario
 
 api_router = APIRouter()
-api_router.include_router(artigo.router, prefix='/cursos', tags=['Cursos'])
+api_router.include_router(artigo.router, prefix='/artigod', tags=['Artigos'])  # /api/v1/artigos
+api_router.include_router(usuario.router, prefix='/usuarios', tags=['Usuários'])    
 
 # /api/v1/cursos
