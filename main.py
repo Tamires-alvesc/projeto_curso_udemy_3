067439802e4148efad8ee3f2723ab345276173b8
Tamires-alvesc <ttamiresacosta@hotmail.com>
2025-10-19 16:36:56 -0300

    arquivo main ok

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from fastapi import FastAPI
 from core.configs import settings
 from api.v1.api import api_router
 
-app = FastAPI(title="Cursos API - FastAPI SQLAlchemy")
+app = FastAPI(title="Cursos API - Segurança e Autenticação", version="1.0.0")
 app.include_router(api_router, prefix=settings.API_V1_STR)
-
-# /api/v1/cursos
 
 if __name__ == "__main__":
     import uvicorn
